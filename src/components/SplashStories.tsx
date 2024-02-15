98ec9c1369a5eeb385d9b9e417c8caf65cc89860
Dmytro Hula <guladmitro261@gmail.com>
2024-02-15 04:59:26 +0100

    Commit: best version of Splash Stories 2.0

--- src/components/SplashStories.tsx
+++ src/components/SplashStories.tsx
@@ -1,6 +1,7 @@
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import Carousel from 'react-native-reanimated-carousel';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+
 
 export default function SplashStories() {
     const welcomeStories: string[] = [
@@ -13,27 +14,65 @@ export default function SplashStories() {
     const [changedIndex, setChangedIndex] = useState(0)
 
     return (
+        // <View style={styles.storiesDiv}>
+        //     <View style={styles.paginationStories}>
+        //         {
+        //             welcomeStories?.map((element, index) => {
+        //                 return changedIndex === index ? 
+        //                 <View key={index} style={{ backgroundColor: '#E0FE10', width: 35, height: 6, borderRadius: 20 }} /> : <View key={index} style={styles.pageStories} />
+        //             })
+        //         }
+        //     </View>
+        //     <Carousel
+        //         loop
+        //         width={width}
+        //         height={150}
+        //         autoPlay={true}
+        //         data={welcomeStories}
+        //         scrollAnimationDuration={1000}
+        //         onSnapToItem={(index) => setChangedIndex(index-1)}
+        //         renderItem={({ index }) =>(
+        //             <View style={{alignItems: 'center'}}>
+        //                 <Text style={styles.bottomDivText}>
+        //                     { welcomeStories[index] }
+        //                 </Text>
+        //             </View>
+        //         )}
+        //     />
+        // </View>
         <View style={styles.storiesDiv}>
-            <View style={styles.paginationStories}>
-                {
-                    welcomeStories?.map((element, index) => {
-                        return changedIndex === index ? 
-                        <View key={index} style={{ backgroundColor: '#E0FE10', width: 35, height: 6, borderRadius: 20 }} /> : <View key={index} style={styles.pageStories} />
-                    })
-                }
-            </View>
+            <Pagination
+                activeDotIndex={changedIndex}
+                containerStyle={{ paddingVertical: 0, paddingBottom: 30, }}
+                dotsLength={welcomeStories.length}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.5}
+                dotStyle={{
+                    width: 28,
+                    height: 7,
+                    borderRadius: 20,
+                    marginHorizontal: -5,
+                    backgroundColor: '#E0FE10'
+                }}
+                inactiveDotStyle={{
+                    width: 20,
+                    height: 7,
+                    borderRadius: 20,
+                    backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                }}
+            />
             <Carousel
-                loop
-                width={width}
-                height={150}
-                autoPlay={true}
                 data={welcomeStories}
-                scrollAnimationDuration={1000}
+                layout={'default'}
+                loop={true}
+                sliderWidth={width}
+                autoplay={true}
                 onSnapToItem={(index) => setChangedIndex(index)}
-                renderItem={({ index }) =>(
-                    <View style={{alignItems: 'center'}}>
+                itemWidth={width}
+                renderItem={({ item, index }) => (
+                    <View style={{ alignItems: 'center' }}>
                         <Text style={styles.bottomDivText}>
-                            { welcomeStories[index] }
+                            {item}
                         </Text>
                     </View>
                 )}
@@ -46,20 +85,20 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        gap: 40,
+        height: 150,
     },
-    paginationStories: {
-        display: 'flex',
-        flexDirection: 'row',
-        gap: 4,
-        alignItems: 'center',
-    },
-    pageStories: {
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        width: 20,
-        height: 6,
-        borderRadius: 20,
-    },
+    // paginationStories: {
+    //     display: 'flex',
+    //     flexDirection: 'row',
+    //     gap: 1,
+    //     alignItems: 'center',
+    // },
+    // pageStories: {
+    //     backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    //     width: 20,
+    //     height: 6,
+    //     borderRadius: 20,
+    // },
     bottomDivText: {
         width: "90%",
         textAlign: 'center',
