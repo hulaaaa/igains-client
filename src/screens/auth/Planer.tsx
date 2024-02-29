78ecc3f859b866d816ac9ff7eb4ad416b9747f2e
Dmytro Hula <guladmitro261@gmail.com>
2024-02-29 09:23:41 +0100

    Commit: Business logic - Calendar, in Planer

--- src/screens/auth/Planer.tsx
+++ src/screens/auth/Planer.tsx
@@ -1,4 +1,5 @@
-import { Dimensions, RefreshControl, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import moment from 'moment';
+import { Alert, Dimensions, RefreshControl, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import Tabs from '../../components/Tabs'
 import { useCallback } from 'react';
@@ -36,7 +37,61 @@ export default function Planer() {
     setTimeout(() => {
       setRefreshing(false);
     }, 800);
-};
+  };
+  
+  let calendarData = []
+  let calendarDataMonth = []
+  const [activeDay, setActiveDay] = useState(moment().format('DD'));
+  const [selectI, setSelectI] = useState(0)
+  let repeatLoop = 0
+  for(let i = 0; i <= 2; i++){
+    const fourMonth = moment().add(i, 'month').calendar({sameDay: 'YYYY-MM',nextDay: 'YYYY-MM',nextWeek: 'YYYY-MM',lastDay: 'YYYY-MM',lastWeek: 'YYYY-MM',sameElse: 'YYYY-MM'})
+    const countDay = moment(fourMonth, "YYYY-MM").daysInMonth();
+    repeatLoop=countDay+repeatLoop
+  }
+  for(let k = 0; k <= repeatLoop; k++){
+      calendarData.push({
+        date: moment().add(k, 'day').calendar({
+          sameDay: 'DD',
+          nextDay: 'DD',
+          nextWeek: 'DD',
+          lastDay: 'DD',
+          lastWeek: 'DD',
+          sameElse: 'DD'
+        }),
+        month: moment().add(k, 'day').calendar({
+          sameDay: 'MMMM',
+          nextDay: 'MMMM',
+          nextWeek: 'MMMM',
+          lastDay: 'MMMM',
+          lastWeek: 'MMMM',
+          sameElse: 'MMMM'
+        }),
+        day: moment().add(k, 'day').calendar({
+          sameDay: 'ddd',
+          nextDay: 'ddd',
+          nextWeek: 'ddd',
+          lastDay: 'ddd',
+          lastWeek: 'ddd',
+          sameElse: 'ddd'
+        }),
+      })
+  }
+  for(let j = 0; j < 3; j++){
+    calendarDataMonth.push({
+      month: moment().add(j, 'month').calendar({
+        sameDay: 'MMMM',
+        nextDay: 'MMMM',
+        nextWeek: 'MMMM',
+        lastDay: 'MMMM',
+        lastWeek: 'MMMM',
+        sameElse: 'MMMM'
+      })
+    })
+  }
+  const [selectedMonth, setSelectedMonth] = useState(calendarDataMonth[selectI].month)
+
+
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
       <SafeAreaView>
@@ -56,167 +111,121 @@ export default function Planer() {
               justifyContent: 'space-between',
               width: Dimensions.get('window').width - 50,
             }}>
-              <TouchableOpacity style={{padding: 10,}}>
-                <Svg width={10} height={16} fill="none">
-                  <Path
-                    stroke="#fff"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={1.5}
-                    d="M8.5 1 1 8.5 8.5 16"
-                  />
-                </Svg>
-              </TouchableOpacity>
+              {
+                selectedMonth!=calendarDataMonth[0].month?(
+                  <TouchableOpacity onPress={()=>{
+                    if (selectI > 0) {
+                      setSelectI(prev=>prev-1)
+                      setActiveDay('')
+                      setSelectedMonth(calendarDataMonth[selectI - 1].month)
+                    }
+                  }} style={{padding: 10,}}>
+                    <Svg width={10} height={16} fill="none">
+                      <Path
+                        stroke="#fff"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.5}
+                        d="M8.5 1 1 8.5 8.5 16"
+                      />
+                    </Svg>
+                  </TouchableOpacity>
+                ):(
+                  <TouchableOpacity style={{padding: 10,}}>
+                    <Svg width={10} height={16} fill="none">
+                      <Path
+                        stroke="transparent"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.5}
+                        d="M8.5 1 1 8.5 8.5 16"
+                      />
+                    </Svg>
+                  </TouchableOpacity>
+                )
+              }
               <Text style={{
                 color: '#FFFFFF',
                 fontFamily: 'Light',
                 fontSize: 18,
+                padding: 10,
               }}>
-                February 
+                {selectedMonth} 
               </Text>
-              <TouchableOpacity>
-                <Svg width={10} height={16} fill="none">
-                  <Path
-                    stroke="#fff"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={1.5}
-                    d="M1.5 16 9 8.5 1.5 1"
-                  />
-                </Svg>
-              </TouchableOpacity>
+              {
+                selectedMonth!=calendarDataMonth[2].month?(
+                  <TouchableOpacity onPress={()=>{
+                    if (selectI < calendarDataMonth.length - 1) {
+                      setActiveDay('')
+                      setSelectI(prev=>prev+1)
+                      setSelectedMonth(calendarDataMonth[selectI + 1].month)
+                    }
+                  }} style={{padding: 10,}}>
+                    <Svg width={10} height={16} fill="none">
+                      <Path
+                        stroke="#fff"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.5}
+                        d="M1.5 16 9 8.5 1.5 1"
+                      />
+                    </Svg>
+                  </TouchableOpacity>
+                ):(
+                  <TouchableOpacity style={{padding: 10,}}>
+                    <Svg width={10} height={16} fill="none">
+                      <Path
+                        stroke="transparent"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.5}
+                        d="M1.5 16 9 8.5 1.5 1"
+                      />
+                    </Svg>
+                  </TouchableOpacity>
+                )
+              }
+              
             </View>
           </View>
 
           {/* SELECT DATE */}
             <View style={styles.selectDate}>
               <ScrollView showsHorizontalScrollIndicator={false} horizontal style={{ width: Dimensions.get('window').width - 50,}}>
-                <TouchableOpacity>
-                  <View style={{marginRight: 4, paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Mon
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      26
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={{marginRight: 4,paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Tue
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      27
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={{marginRight: 4,paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Wen
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      28
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={{marginRight: 4,paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Thu
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      29
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={{marginRight: 4,paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Fri
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      30
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={{marginRight: 4,paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Sat
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      31
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={{paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}}>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 12,
-                    }}>
-                      Sat
-                    </Text>
-                    <Text style={{
-                      color: '#FFFFFF',
-                      fontFamily: 'Light',
-                      fontSize: 15,
-                    }}>
-                      31
-                    </Text>
-                  </View>
-                </TouchableOpacity>
+                {
+                  calendarData.filter(item=>item.month==selectedMonth).map((item, index) => (
+                    <TouchableOpacity onPress={()=>{
+                      setActiveDay(item.date)
+                    }} key={index}>
+                      <View style={activeDay!=item.date?{marginRight: 4, paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#17181B',gap:5, alignItems: 'center',justifyContent:'center'}:{marginRight: 4, paddingHorizontal:15, paddingVertical:9, borderRadius: 12, backgroundColor:'#E0FE10',gap:5, alignItems: 'center',justifyContent:'center'}}>
+                        <Text style={activeDay!=item.date?{
+                          color: '#FFFFFF',
+                          fontFamily: 'Light',
+                          fontSize: 12,
+                        }:{
+                          color: '#17181B',
+                          fontFamily: 'Light',
+                          fontSize: 12,
+                        }}>
+                          {item.day}
+                        </Text>
+                        <Text style={activeDay!=item.date?{
+                          color: '#FFFFFF',
+                          fontFamily: 'Light',
+                          fontSize: 12,
+                        }:{
+                          color: '#17181B',
+                          fontFamily: 'Light',
+                          fontSize: 15,
+                        }}>
+                          {item.date}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))
+                }
+                
+                
               </ScrollView>
             </View>
 
